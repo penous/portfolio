3c466c37c3afb53c02d547da5e6e8a86c3338ff5
Tom Dirinck <tom.dirinck@gmail.com>
2021-09-12 17:57:52 +0200

    Updated file name.

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { skills } from '../context/skills';
+import { skills } from '../context/skillsData';
 import SkillItem from './skillItem';
 
 const Skills = () => {
