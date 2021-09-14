657d872d0646c37f9c0130b5ebf98352267fa764
Tom Dirinck <tom.dirinck@gmail.com>
2021-09-14 15:07:54 +0200

    Add favicon.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 import About from '../components/about';
 import Contact from '../components/contact';
 import Main from '../components/main';
@@ -8,6 +9,21 @@ import Skills from '../components/skills';
 const IndexPage = () => {
   return (
     <Fragment>
+      <Helmet>
+        <meta name='icon' href='../images/favicon.ico' />
+        <meta name='icon' href='../images/nav_logo.png' />
+
+        <link
+          href='../images/favicon.ico'
+          rel='shortcut icon'
+          type='image/x-icon'
+        />
+        <link
+          href='../images/nav-logo.png'
+          rel='shortcut icon'
+          type='image/x-icon'
+        />
+      </Helmet>
       <Main />
       <About />
       <Skills />
