4ffefdfd777542a54f6290104307e05323b1cc90
Tom Dirinck <tom.dirinck@gmail.com>
2021-09-16 15:48:12 +0200

    Play on mobile.

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Video = ({ source }) => {
   return (
-    <video autoPlay='true' loop='true' muted>
+    <video playsInline='true' autoPlay='true' loop='true' muted>
       <source src={source} type='video/mp4' />
     </video>
   );
